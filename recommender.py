d7062c1fd77b8854a13f02210718e9aead20aed0
sergeuts <92944373+sergeuts@users.noreply.github.com>
2021-11-22 16:57:30 +0300

    Add files via upload

--- recommender.py
+++ recommender.py
@@ -1,9 +1,45 @@
+''' Для построения рекомендательной системы выбрана библиотека surprise (surprise.readthedocs.io)
+    Датасет данной библиотеки используют таблицу рейтингов из 3-х колонок user-item-rating.
+    Для подготовки этих данных используется файл transactions.csv 
+    Для расчета предсказаний выбран алгоритм cингулярного разложения SVD (Singular value decomposition)
+    для экономии времени при повторных расчетах реализован механизм кеширования.
+    После первоначального обучения алгоритма его расчетные данные записываются на диск (методом класса dump из этой же библиотеки).
+    Повторный перерасчет алгоритма происходит если не найден его дамп или если расчетные данные устарели (при сравнении времени файла
+     с временем файла транзакций). При расчете прогноза на больших входных данных (например по всем пользователям) используется 
+     буферизованный вывод в файл. По умолчанию размер буфера установлен для расчета 1000 пользователей (всего в датасете 100 000 пользователей). 
+
+     Из дополнительных возможностей в тех. задании реализованы следующие функции:
+
+    - По идентификатору пользователя выдавать набор из K наиболее релевантных для него товаров.
+    - По массиву идентификаторов пользователей выдавать массив наборов из K наиболее релевантных для них товаров.
+        При вызове функции predict() можно можно передавать id пользователей как строкой, так и списком.
+        Например, predict(usr='7', k=10), predict(usr='10 22 42', k=15), predict(usr=['13', '22', '42'], k=4)
+        В зависимости от типа параметра и количества пользователей система, вернет либо обычный одномерный список,
+        рекомендуемых товаров, либо вложенный список для нескольких пользователей вида [['user_id', ['id1','id2'...]]]:
+        [['13',['46', '117', '169', '264']],
+         ['22',['695', '738', '1729', '1835']],
+         ['42',['196', '377', '432', '1096']]]
+        Если функцию predict() запустить без параметров, то она произведёт расчёт по всем пользователям и результаты
+        запишет в файл submit.csv (в рабочем каталоге) в формате:
+            user_id - уникальный идентификатор покупателя
+            product_id - идентификаторы товаров через пробел в порядке убывания "уверенности модели"
+    
+    - Добавить свежие данные о транзакциях.
+    - Обучить рекомендательную систему заново.
+        Обновление данных происходит автоматически при (пере-)запуске системы в процедуре prepare_model().
+        Обновление запуститься при отсутствии файлов сводного рейтинга rank.scv и файла кеша обученной модели algo.dmp.
+    - Система уведомлений о состоянии алгоритма расчета и выполнении отдельных этапов. Уведомления можно отключать  .
+    - Автоматическое возобновления прерванных длительных расчетов с позиции последнего сохранения буфера.
+    - Реализован замер времени (можно отключать) для отладки и оптимизации.
+'''
+
 from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
 from surprise import dump
 import os
 import csv
+import time
 
 # файлы данных, результаты и кеш
 data_dir      = os.path.join(os.getcwd(), 'skillbox-recommender-system')
@@ -15,14 +51,16 @@ algo_file     = os.path.join(data_dir, 'algo.dmp')
 # status_file содержит id последнего сохраненного расчитанного (user_id) элемента
 # При прерывании и возобновлении расчета позволяет сэкономить время
 # и начать расчет сразу со следующего элемента
-status_file   = os.path.join(data_dir, 'status.log')
+status_file   = os.path.join(data_dir, 'status2.log')
 # файл с результатами
-result_file   = os.path.join(data_dir, 'submit.csv')
+result_file   = os.path.join(data_dir, 'submit2.csv')
 
 print_log = True # выводить на экран процесс выполнения алгоритма
+measure_time = True # замерять и выводить время работы
 algo = [] # surprise.prediction_algorithms.matrix_factorization.SVD
-user_list = []
-item_list = []
+user_list = [] # все user_id, которые есть в транзакциях
+item_list = [] # все item_id, которые есть в транзакциях
+
 
 # Функция make_rank_file формирует file2.csv на основе группировки данных из file1
 # по заданным колонкам cols, используя при парсинге разделители sep.
@@ -59,15 +97,18 @@ def make_rank_file(file1='', file2='', cols='1 6', sum_cols='', sep=',', pass_he
         for item in summedup_list:
             writer.writerow(item)
 
+
 def prepare_model():
     if not os.path.exists(status_file):
         f = open(status_file, 'w', encoding='UTF-8')
         f.write('done')
+        #f.write('processing 75000')
         f.close()
     global algo, user_list, item_list
     if os.path.exists(algo_file):
+        if print_log: print('log: Загружается модель...')
         (user_list, item_list), algo = dump.load(algo_file)
-        if print_log: print('log: Загружена из кеша обученная модель.')
+        if print_log: print('log: Модель загружена.')
         return True
 
     if not os.path.exists(rank_file):
@@ -101,14 +142,31 @@ def prepare_model():
     if print_log: print('log: Выполняется обучение модели...')
     algo.fit(trainset)
     dump.dump(algo_file, algo=algo, predictions=(user_list, item_list))
-    if print_log: print('log: Данные обученной модели сохранены в кеш.')
+    if print_log: print('log: Данные обученной модели сохранены в кеш (algo.dmp).')
     if print_log: print('log: Модель готова.')
     return True
 
-def predict(usr=[], k=10):
-    if not prepare_model():
+
+def predict(usr=[], k=10, save_file=True, n_factors=0):
+    global algo
+    if algo == []:
+        print('Ошибка: Не инициализирована модель алгоритма. Выполните сначала процедуру prepare_model().')
         return
+
+    if n_factors:
+        algo.n_factors = n_factors
+
+    if len(usr):
+        if type(usr) == str:
+            usr = usr.split(' ')
+        save_file = False    
+    else:
+        usr = user_list        
+
     if print_log: print('log: Выполняется расчёт предсказаний...')
+    if measure_time:
+        time_start = time.time()
+        time_start1 = time.time()
     buffer_size = 1000
     buf_count = 0
     buffer = []
@@ -116,12 +174,15 @@ def predict(usr=[], k=10):
     txt = stat_file.read()
     stat_file.close()
     if txt.find('processing') >= 0:
-        cur_line = int(txt.splitlines[0].split[1])
+        cur_line = int(txt.split(' ')[1])
     else:
         cur_line = -1
-    for indx, u in enumerate(user_list):
+    for indx, u in enumerate(usr):
         if cur_line >= indx:
             continue
+        if indx == 0:
+            with open(result_file, 'a') as res_file:
+                res_file.writelines('user_id,product_id\n')
         test_set = []
         for i in item_list:
             test_set.append((u, i, 0))
@@ -129,21 +190,26 @@ def predict(usr=[], k=10):
         predictions.sort(key=lambda x: x.est, reverse=True)
         buffer.append((u, [v.iid for v in predictions[:k]]))
         buf_count += 1
-        if buf_count >= buffer_size:
+        if buf_count >= buffer_size and save_file:
             txt = ''
             for b in buffer:
-                txt += b[0] + '\n' + ' '.join(b[1]) + '\n'
+                txt += b[0] + ',' + ' '.join(b[1]) + '\n'
             with open(result_file, 'a') as res_file:
                 res_file.writelines(txt)
             with open(status_file, 'w') as stat_file:
                 stat_file.write(f'processing {indx}')
             if print_log: print(f'log: Выполнено {indx}.')
             buf_count = 0
-            buffer = []    
-    if buffer:
+            buffer = []
+            if measure_time:
+                time_end1 = time.time()
+                print('Время {:.2f} сек'.format(time_end1-time_start1))
+                time_start1 = time.time()
+
+    if buffer and save_file:
         txt = ''
         for b in buffer:
-            txt += b[0] + '\n' + ' '.join(b[1]) + '\n'
+            txt += b[0] + ',' + ' '.join(b[1]) + '\n'
         with open(result_file, 'a') as res_file:
             res_file.writelines(txt)
         with open(status_file, 'w') as stat_file:
@@ -151,43 +217,17 @@ def predict(usr=[], k=10):
         buf_count = 0
         buffer = []    
     if print_log: print('log: Расчет выполнен.')
+    if measure_time:
+        time_end = time.time()
+        print('Время {:.2f} сек'.format(time_end-time_start))
+
+    if not save_file:
+        if len(buffer) == 1:
+            return buffer[0][1]
+        elif len(buffer) > 1:
+            return buffer
+
     
 if __name__ == '__main__':
-    ''' Для построения рекомендательной системы выбрана библиотека surprise (surprise.readthedocs.io)
-    Датасет данной библиотеки используют таблицу рейтингов из 3-х колонок user-item-rating.
-    Для подготовки этих данных используется файл transactions.csv 
-    Для расчета предсказаний выбран алгоритм cингулярного разложения SVD (Singular value decomposition)
-    для экономии времени при повторных расчетах реализован механизм кеширования.
-    После первоначального обучения алгоритма его расчетные данные записываются на диск (методом класса dump из этой же библиотеки).
-    Повторный перерасчет алгоритма происходит если не найден его дамп или если расчетные данные устарели (при сравнении времени файла
-     с временем файла транзакций). При расчете прогноза на больших входных данных (например по всем пользователям) используется 
-     буферизованный вывод в файл. По умолчанию размер буфера установлен для расчета 1000 пользователей (всего в датасете 100 000 пользователей). 
-
-     Из дополнительных возможностей в тех. задании реализованы следующие функции:
-
-    - По идентификатору пользователя выдавать набор из K наиболее релевантных для него товаров.
-    - По массиву идентификаторов пользователей выдавать массив наборов из K наиболее релевантных для них товаров.
-        При вызове функции predict() можно можно передавать id пользователей как строкой, так и списком.
-        Например, predict(usr='7', k=10), predict(usr='10 22 42', k=15), predict(usr=['13', '22', '42'], k=4)
-        В зависимости от типа параметра и количества пользователей система, вернет либо обычный одномерный список,
-        рекомендуемых товаров, либо вложенный список для нескольких пользователей вида [['user_id', ['id1','id2'...]]]:
-        [['13',['46', '117', '169', '264']],
-         ['22',['695', '738', '1729', '1835']],
-         ['42',['196', '377', '432', '1096']]]
-        Если функцию predict() запустить без параметров, то она произведёт расчёт по всем пользователям и результаты
-        запишет в файл submit.csv (в рабочем каталоге) в формате:
-            user_id - уникальный идентификатор покупателя
-            product_id - идентификаторы товаров через пробел в порядке убывания "уверенности модели"
-    
-    - Добавить свежие данные о транзакциях.
-    - Обучить рекомендательную систему заново.
-        Обновление данных происходит автоматически при (пере-)запуске системы в процедуре prepare_model().
-        Обновление запуститься, если дата и/или время файла транзакций превышает соответствующие данные
-        файла сводного рейтинга rank.scv и файла кеша обученной модели algo.dmp.
-        Обновление запуститься также при отсутствии этих файлов.
-    - Система уведомлений о состоянии алгоритма расчета и выполнении отдельных этапов. Уведомления можно отключать  .
-    - Автоматическое возобновления прерванных длительных расчетов с позиции последнего сохранения буфера.
-    
-
-    '''
-    predict()
+    if prepare_model():
+        predict()
